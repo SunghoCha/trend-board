8cfb1056e5e1fb3818d818be2b39c4a636e17b04
SunghoCha <tjdgh3725@gmail.com>
2026-01-12 07:43:03 +0900

    chore : Spring Boot 초기 프로젝트 생성

---
+++ src/main/java/com/sungho/trendboard/TrendBoardApplication.java
@@ -0,0 +1,13 @@
+package com.sungho.trendboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sungho/trendboard/TrendBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungho.trendboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

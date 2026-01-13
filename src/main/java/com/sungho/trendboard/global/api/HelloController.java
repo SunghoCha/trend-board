f39a3b30a9514c63b8c783a807a95095edbe5071
SunghoCha <tjdgh3725@gmail.com>
2026-01-14 07:23:04 +0900

    chore : yml 임시 설정 테스트

--- src/main/java/com/sungho/trendboard/global/api/HelloController.java
+++ src/main/java/com/sungho/trendboard/global/api/HelloController.java
@@ -1,2 +1,20 @@
-package com.sungho.trendboard.global.api;public class HelloController {
+package com.sungho.trendboard.global.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "hello";
+    }
 }
